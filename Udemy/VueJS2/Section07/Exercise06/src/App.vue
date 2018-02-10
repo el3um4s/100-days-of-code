c9790aaac981462612da5df38e01487eaac5e22c
el3um4s <el3um4s@gmail.com>
2018-02-10 14:58:09 +0100

    Impostazioni per fare esercizio 6

---
+++ Udemy/VueJS2/Section07/Exercise06/src/App.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>Server Status</h1>
+                </header>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12 col-sm-6">
+                <ul class="list-group">
+                    <li
+                            class="list-group-item"
+                            v-for="index in 5">
+                        Server #{{ index }}
+                    </li>
+                </ul>
+            </div>
+            <div class="col-xs-12 col-sm-6">
+                <p>Server Details are currently not updated</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row">
+            <div class="col-xs-12">
+                <footer>
+                    <p>All Servers are managed here</p>
+                </footer>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
