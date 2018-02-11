ffe67208bc07473ccc702d765ca56f96b0c65e35
el3um4s <el3um4s@gmail.com>
2018-02-12 00:18:28 +0100

    $emit(nomeEmit, datoDaRestituire)

--- Udemy/VueJS2/Section08/Lezioni/src/components/User.vue
+++ Udemy/VueJS2/Section08/Lezioni/src/components/User.vue
@@ -3,10 +3,11 @@
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
         <button @click="changeName">Change my Name</button>
+        <p>Name is {{ name }}</p>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail :myName="name"></app-user-detail>
+                <app-user-detail :myName="name" @nameWasReset="name = $event"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
--- Udemy/VueJS2/Section08/Lezioni/src/components/UserDetail.vue
+++ Udemy/VueJS2/Section08/Lezioni/src/components/UserDetail.vue
@@ -3,6 +3,7 @@
   <h3>You may view the User Details here</h3>
   <p>Many Details</p>
   <p>User Name: {{ switchName() }}</p>
+  <button @click="resetName">Reset name</button>
 </div>
 </template>
 
@@ -16,6 +17,10 @@ export default {
   methods: {
     switchName() {
       return this.myName.split("").reverse().join("");
+    },
+    resetName() {
+      this.myName = 'max';
+      this.$emit('nameWasReset', this.myName);
     }
   }
 }
