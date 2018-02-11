a414f1337710d8b4f105eb49c4716c5271a22ada
el3um4s <el3um4s@gmail.com>
2018-02-11 23:58:48 +0100

    Validating Props

--- Udemy/VueJS2/Section08/Lezioni/src/components/UserDetail.vue
+++ Udemy/VueJS2/Section08/Lezioni/src/components/UserDetail.vue
@@ -1,24 +1,28 @@
 <template>
-    <div class="component">
-        <h3>You may view the User Details here</h3>
-        <p>Many Details</p>
-        <p>User Name: {{ switchName() }}</p>
-    </div>
+<div class="component">
+  <h3>You may view the User Details here</h3>
+  <p>Many Details</p>
+  <p>User Name: {{ switchName() }}</p>
+</div>
 </template>
 
 <script>
-  export default {
-    props: ['myName'],
-    methods: {
-      switchName(){
-        return this.myName.split("").reverse().join("");
-      }
+export default {
+  props: {
+    myName: {
+      type: String
+    }
+  },
+  methods: {
+    switchName() {
+      return this.myName.split("").reverse().join("");
     }
   }
+}
 </script>
 
 <style scoped>
-    div {
-        background-color: lightcoral;
-    }
+div {
+  background-color: lightcoral;
+}
 </style>
