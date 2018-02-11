27fa046be84c799624cf5916070a0e0d1d298183
el3um4s <el3um4s@gmail.com>
2018-02-11 23:51:42 +0100

    Props

--- Udemy/VueJS2/Section08/Lezioni/src/components/User.vue
+++ Udemy/VueJS2/Section08/Lezioni/src/components/User.vue
@@ -2,10 +2,11 @@
     <div class="component">
         <h1>The User Component</h1>
         <p>I'm an awesome User!</p>
+        <button @click="changeName">Change my Name</button>
         <hr>
         <div class="row">
             <div class="col-xs-12 col-sm-6">
-                <app-user-detail></app-user-detail>
+                <app-user-detail :myName="name"></app-user-detail>
             </div>
             <div class="col-xs-12 col-sm-6">
                 <app-user-edit></app-user-edit>
@@ -19,6 +20,16 @@
     import UserEdit from './UserEdit.vue';
 
     export default {
+      data: function() {
+        return {
+          name: 'Max'
+        }
+      },
+      methods: {
+        changeName(){
+          this.name = 'Anna'
+        }
+      },
         components: {
             appUserDetail: UserDetail,
             appUserEdit: UserEdit
--- Udemy/VueJS2/Section08/Lezioni/src/components/UserDetail.vue
+++ Udemy/VueJS2/Section08/Lezioni/src/components/UserDetail.vue
@@ -2,10 +2,19 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>User Name: {{ switchName() }}</p>
     </div>
 </template>
 
 <script>
+  export default {
+    props: ['myName'],
+    methods: {
+      switchName(){
+        return this.myName.split("").reverse().join("");
+      }
+    }
+  }
 </script>
 
 <style scoped>
