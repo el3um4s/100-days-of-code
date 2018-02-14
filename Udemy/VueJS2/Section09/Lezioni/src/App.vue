c771f53af86a3e618e2ee2a6a3ba76b096f9b601
el3um4s <el3um4s@gmail.com>
2018-02-14 21:33:22 +0100

    slot

---
+++ Udemy/VueJS2/Section09/Lezioni/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+              <appQuote>
+                <h2 slot="title">{{ quoteTitle }}</h2>
+                <p>A wonderful Quote</p>
+              </appQuote>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+  import Quote from './components/Quote.vue';
+
+    export default {
+      data: function(){
+        return {
+          quoteTitle: 'The Quote'
+        }
+      },
+        components: {
+          appQuote: Quote
+        }
+    }
+</script>
+
+<style scoped>
+</style>
