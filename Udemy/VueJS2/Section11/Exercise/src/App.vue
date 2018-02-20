f35c80098c95520b1e72728bff100563f87d0720
el3um4s <el3um4s@gmail.com>
2018-02-20 23:52:02 +0100

    impostato bootstrapt

--- Udemy/VueJS2/Section11/Exercise/src/App.vue
+++ Udemy/VueJS2/Section11/Exercise/src/App.vue
@@ -1,48 +1,59 @@
 <template>
-    <div class="container">
-        <form>
-            <div class="row">
-                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                    <!-- Exercise 1 -->
-                    <!-- Create a Signup Form where you retrieve the following Information -->
-                    <!-- Full Name (First Name + Last Name) -->
-                    <!-- Mail -->
-                    <!-- Password -->
-                    <!-- Store Data? Yes/No -->
-
-                    <!-- Exercise 2 -->
-                    <!-- Only display the Form if it has NOT been submitted -->
-                    <!-- Display the Data Summary ONCE the Form HAS been submitted -->
-
-                    <!-- Exercise 3 -->
-                    <!-- Edit the Example from above and create a custom "Full Name" Control -->
-                    <!-- which still holds the First Name and Last Name Input Field -->
-                </div>
-            </div>
-        </form>
-        <hr>
-        <div class="row">
-            <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4>Your Data</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
-                    </div>
-                </div>
-            </div>
-        </div>
+<div class="d-flex justify-content-around">
+  <div class="card align-self-center" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Signup Form</h5>
+      <div></div>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
     </div>
+  </div>
+  <div class="card align-self-center" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">Your Data</h5>
+      <div>
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item">Cras justo odio</li>
+          <li class="list-group-item">Dapibus ac facilisis in</li>
+          <li class="list-group-item">Vestibulum at eros</li>
+        </ul>
+      </div>
+      <a href="#" class="btn btn-primary">Go somewhere</a>
+    </div>
+  </div>
+</div>
 </template>
 
 <script>
-    export default {
-    }
+export default {}
 </script>
 
 <style>
+
 </style>
+
+
+
+
+<!-- Exercise 1 -->
+<!-- Create a Signup Form where you retrieve the following Information -->
+<!-- Full Name (First Name + Last Name) -->
+<!-- Mail -->
+<!-- Password -->
+<!-- Store Data? Yes/No -->
+
+<!-- Exercise 2 -->
+<!-- Only display the Form if it has NOT been submitted -->
+<!-- Display the Data Summary ONCE the Form HAS been submitted -->
+
+<!-- Exercise 3 -->
+<!-- Edit the Example from above and create a custom "Full Name" Control -->
+<!-- which still holds the First Name and Last Name Input Field -->
+
+    <!-- <h4>Your Data</h4>
+
+<div class="panel-body">
+    <p>Full Name: </p>
+    <p>Mail: </p>
+    <p>Password: </p>
+    <p>Store in Database?: </p>
+</div> -->
