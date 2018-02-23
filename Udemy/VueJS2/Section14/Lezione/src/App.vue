21c5d75bbe65bbdfe6098e79a9e0100e76d96bb7
el3um4s <el3um4s@gmail.com>
2018-02-23 19:59:52 +0100

    transition

--- Udemy/VueJS2/Section14/Lezione/src/App.vue
+++ Udemy/VueJS2/Section14/Lezione/src/App.vue
@@ -3,7 +3,15 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button type="button" name="button" class="btn btn-primary" @click="show = !show"> Show Alert </button>
+                <br><br>
+                <transition name="fade">
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                </transition>
+                <transition name="slide">
+                    <div class="alert alert-info" v-if="show">This is some Info</div>
+                </transition>
             </div>
         </div>
     </div>
@@ -13,12 +21,54 @@
     export default {
         data() {
             return {
-
+              show:false
             }
         }
     }
 </script>
 
 <style>
+  .fade-enter{
+    opacity: 0;
+  }
+  .fade-enter-active{
+    transition: opacity 1s;
+  }
+  .fade-leave{
 
+  }
+  .fade-leave-active{
+    transition: opacity 1s;
+    opacity: 0;
+  }
+
+  .slide-enter{
+
+  }
+  .slide-enter-active{
+    animation: slide-in 1s ease-out forwards;
+  }
+  .slide-leave{
+
+  }
+  .slide-leave-active{
+    animation: slide-out 1s ease-out forwards;
+  }
+
+  @keyframes slide-in {
+    from {
+      transform: translateY(20px);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
+  @keyframes slide-out {
+      from {
+        transform: translateY(0);
+      }
+      to {
+        transform: translateY(20px);
+      }
+  }
 </style>
