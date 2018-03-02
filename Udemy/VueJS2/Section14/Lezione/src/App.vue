7ff6fc3bc93c0ad52f08f41466e5942706c6d48c
el3um4s <el3um4s@gmail.com>
2018-03-02 15:44:08 +0100

    Finita section 4

--- Udemy/VueJS2/Section14/Lezione/src/App.vue
+++ Udemy/VueJS2/Section14/Lezione/src/App.vue
@@ -55,6 +55,22 @@
       <transition name="fade" mode="out-in">
         <component :is="selectedComponent"></component>
       </transition>
+      <hr>
+      <button class="btn btn-primary" @click="addItem">Add Item</button>
+      <br><br>
+      <ul class="list-group">
+        <transition-group name="slide">
+              <li
+                class="list-group-item"
+                v-for="(number, index) in numbers"
+                @click="removeItem(index)"
+                style="cursor: pointer;"
+                :key="number">
+                {{ number }}
+              </li>
+          </transition-group>
+      </ul>
+
     </div>
   </div>
   <div class="row">
@@ -116,7 +132,8 @@ export default {
       showAnimation: true,
       load: true,
       elementWidth: 100,
-      selectedComponent: 'app-success-alert'
+      selectedComponent: 'app-success-alert',
+      numbers: [1,2,3,4,5,6]
     }
   },
   computed: {
@@ -173,6 +190,13 @@ export default {
     },
     leaveCancelled(el){
       console.log('leaveCancelled');
+    },
+    addItem(){
+      const pos = Math.floor(Math.random() * this.numbers.length);
+      this.numbers.splice(pos, 0, this.numbers.length+1)
+    },
+    removeItem(index){
+      this.numbers.splice(index,1);
     }
   }
 }
@@ -209,6 +233,11 @@ export default {
   animation: slide-out 1s ease-out forwards;
   transition: opacity 1s;
   opacity: 0;
+  position: absolute;
+}
+
+.slide-move {
+  transition: transform 1s;
 }
 
 @keyframes slide-in {
