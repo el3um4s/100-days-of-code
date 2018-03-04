4cc8e3478ad22be4db11ddae14c50ca42bf4831c
el3um4s <el3um4s@gmail.com>
2018-03-04 12:13:45 +0100

    params & props

--- Udemy/VueJS2/Section16/Lezione/src/components/Header.vue
+++ Udemy/VueJS2/Section16/Lezione/src/components/Header.vue
@@ -1,7 +1,8 @@
 <template>
   <ul class="nav nav-pills">
     <router-link to="/" tag="li" active-class="active" exact><a>Home</a></router-link>
-    <router-link to="/user/10" tag="li" active-class="active"><a>User</a></router-link>
+    <router-link to="/user/1" tag="li" active-class="active"><a>User 1</a></router-link>
+    <router-link to="/user/2" tag="li" active-class="active"><a>User 2</a></router-link>
 </ul>
 </template>
 
--- Udemy/VueJS2/Section16/Lezione/src/components/user/User.vue
+++ Udemy/VueJS2/Section16/Lezione/src/components/user/User.vue
@@ -9,11 +9,19 @@
 
 <script>
   export default {
-    data(){
-      return {
-        id: this.$route.params.id
-      }
+    props: {
+      id: String
     },
+    // data(){
+    //   return {
+    //     id: this.$route.params.id
+    //   }
+    // },
+    // watch: {
+    //   '$route'(to, from){
+    //     this.id = to.params.id;
+    //   }
+    // },
     methods: {
       navigateToHome(){
         this.$router.push('/');
